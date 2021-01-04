04a9449a8f782f49cd0344a2f48c836262dcdb96
Sean Marjason <smarj1@hotmail.com>
2021-01-04 18:01:06 +0000

    Convert seconds to minutes and seconds in track navigator

--- trackNavigator.js
+++ trackNavigator.js
@@ -18,22 +18,23 @@ function TrackNavigator(){
     line(width - 20, this.y - 5, width - 20, this.y + 5)
 
     // Track Time Elapsed & Remaining
-    let trackDuration = sound.duration().toFixed(0);
-    let trackElapsed = soundTime.toFixed(0);
-    let trackRemaining = (trackDuration - trackElapsed).toFixed(0);
+    let trackDuration = sound.duration();
+    let trackElapsed = soundTime;
+    let trackRemaining = (trackDuration - trackElapsed);
 
     strokeWeight(1);
-    text((trackElapsed), this.x, this.y + 20);
+    text(convertSecondsToMinutes(trackElapsed), this.x, this.y + 20);
     textAlign(RIGHT);
-    text(("-" + (trackRemaining)), width - 20, this.y + 20)
-
+    text(("-" + convertSecondsToMinutes(trackRemaining)), width - 20, this.y + 20)
     pop();
   }
 
   // convert seconds to minutes and seconds
   let convertSecondsToMinutes = function(seconds) {
-    let elapsedMinutes = Math.floor(soundTime / 60);
-    let elapsedSeconds = soundTime % 60;
-    return (elapsedMinutes.toString() + '.' + elapsedSeconds.toFixed(2).toString());
+    let elapsedMinutes = (Math.floor(seconds / 60)).toFixed();
+    let elapsedSeconds = (seconds % 60).toFixed();
+    return  (elapsedSeconds < 10 ? 
+            (elapsedMinutes + '.0' + elapsedSeconds) 
+            : (elapsedMinutes + '.' + elapsedSeconds));
   }
 }
