014567c361a99523da350f9266bada72f616e6de
Sean Marjason <smarj1@hotmail.com>
2021-01-07 15:44:49 +0000

    Update songs for track selector

--- trackSelector.js
+++ trackSelector.js
@@ -4,25 +4,23 @@ function TrackSelector(){
   // set up mapping between selector label and path to audio file
   let tracks = {
     'Default': 'assets/stomper_reggae_bit.mp3',
-    'Ed Sheeran - Castle On The Hill': 'assets/EdSheeran_CastleOnTheHill.mp3',
-    'Rudimental - These Days': 'assets/Rudimental_TheseDays.mp3',
-    'Rufus - Treat You Better': 'assets/RufusDuSol_TreatYouBetter.mp3',
-    'Stormzy - Rainfall': 'assets/Stormzy_Rainfall.mp3',
-    'Swedish House Mafia - Save The World': 'assets/SwedishHouseMafia_SaveTheWorld.mp3',
+    'Dubstep': 'assets/bensound-dubstep.mp3',
+    'Epic': 'assets/bensound-epic.mp3',
+    'Hip Hop': 'assets/bensound-groovyhiphop.mp3',
+    'Taiko': 'assets/bensound-instinct.mp3',
+    'Ukulele': 'assets/bensound-ukulele.mp3',
   }
 
   this.x = 80;
   this.y = 25;
+  this.width = 250;
+  this.height = 25;
 
   let sel;
   sel = createSelect();
   sel.position(this.x, this.y);
-  sel.option('Default');
-  sel.option('Ed Sheeran - Castle On The Hill');
-  sel.option('Rudimental - These Days');
-  sel.option('Rufus - Treat You Better');
-  sel.option('Stormzy - Rainfall');
-  sel.option('Swedish House Mafia - Save The World');
+  sel.size(this.width, this.height);
+  Object.keys(tracks).forEach((item) => sel.option(item));
 
   sel.changed(function() {
     controls.playbackButton.enabled = false;
