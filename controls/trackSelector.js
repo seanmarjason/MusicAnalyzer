90259508ab9d20c5a6f5dff070eef48ee13563a3
Sean Marjason <smarj1@hotmail.com>
2021-01-10 19:43:25 +0000

    Adjust let to var in trackSelector

--- controls/trackSelector.js
+++ controls/trackSelector.js
@@ -2,7 +2,7 @@
 function TrackSelector(){
 
   // set up mapping between selector label and path to audio file
-  tracks = {
+  var tracks = {
     'Default': 'assets/stomper_reggae_bit.mp3',
     'Dubstep': 'assets/bensound-dubstep.mp3',
     'Epic': 'assets/bensound-epic.mp3',
@@ -20,7 +20,7 @@ function TrackSelector(){
   this.onResize();
 
   this.setup = function() {
-    let sel;
+    var sel;
     sel = createSelect();
     sel.position(this.x, this.y);
     sel.size(this.width, this.height);
@@ -43,7 +43,7 @@ function TrackSelector(){
   }
 
   // Callback upon successful loading of new audio file
-  let soundLoaded = function(){
+  var soundLoaded = function(){
     controls.playbackButton.enabled = true;
   }
 }
