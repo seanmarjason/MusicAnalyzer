3839597c2b184ea3babab51ae86c7611e6ac5aab
Sean Marjason <smarj1@hotmail.com>
2021-02-07 15:33:08 +0000

    Update clipping and levels visualisations

--- settings/clippingSettings.js
+++ settings/clippingSettings.js
@@ -5,7 +5,7 @@ function ClippingSettings() {
   // Signal Threshold
   var signalThreshold;
   var signalThresholdOptions = [50, 100, 150, 200, 240];
-  var signalThresholdPos = {x: 50, y: 200}
+  var signalThresholdPos = {x: width/2 - 50, y: 200}
 
   // create DOM elements only on show
   // avoids multiple elements created in draw loop
--- visualisations/levels.js
+++ visualisations/levels.js
@@ -22,7 +22,7 @@ function Levels() {
 		this.pad = width / 20;
 		this.plotWidth = (width - this.pad) / this.plotsAcross;
 		this.plotHeight = (height - this.pad) / this.plotsDown;
-		this.dialRadius = (this.plotWidth - this.pad) / 2 - 5;
+		this.dialRadius = (this.plotWidth - this.pad) / 2 - 50;
 	};
 	this.onResize();
 
@@ -46,14 +46,34 @@ function Levels() {
 
 				//draw a rectangle at that location and size
 				rect(x, y, w, h);
+				//add arcs for level guage and max level
+				push();
+				stroke(000000);
+				arc(x + w / 2, y + h, this.dialRadius * 2 - 10, this.dialRadius * 2 - 10, minAngle, maxAngle)
+				stroke('#CC4436');
+				strokeWeight(10);
+				strokeCap(SQUARE);
+				noFill();
+				arc(x + w / 2, y + h, this.dialRadius * 2 - 20, this.dialRadius * 2 - 20, minAngle + (PI / 10 * 6), maxAngle);
+				pop();
 				//add on the ticks
-				this.ticks(x + w / 2, y + h, this.frequencyBins[currentBin]);
+				this.ticks(x + w / 2, y + h);
 
 				var energy = levelsFourier.getEnergy(this.frequencyBins[currentBin]);
+
+				//add label for plot
+				push();
+				textAlign(CENTER);
+				textSize(12);
+				fill(000000);
+				text(this.frequencyBins[currentBin] + ' (amp)', x + (w/2), y + h - 50);
+				pop();
 
 				//add the needle
 				this.drawNeedle(energy, x + w / 2, y + h);
 				currentBin++;
+
+
 			}
 		}
 
@@ -87,7 +107,7 @@ function Levels() {
 	 *@param bottomY: The bottom y coordinate of the plot rectangle
 	 *@param freqLabel: Label denoting the frequency of the plot
 	 */
-	this.ticks = function(centreX, bottomY, freqLabel) {
+	this.ticks = function(centreX, bottomY) {
 		// 8 ticks from pi to 2pi
 		var nextTickAngle = minAngle;
 		push();
@@ -98,21 +118,23 @@ function Levels() {
 		arc(0, 0, 20, 20, PI, 2 * PI);
 		textAlign(CENTER);
 		textSize(12);
-		text(freqLabel + ' (amp)', 0, -(this.plotHeight / 2));
+
 
 		for (var i = 0; i < 9; i++) {
 			//for each tick work out the start and end coordinates of
 			//based on its angle from the needle's origin.
 			var x = this.dialRadius * cos(nextTickAngle);
-			var x1 = (this.dialRadius - 5) * cos(nextTickAngle);
+			var x1 = (this.dialRadius + 5) * cos(nextTickAngle);
 
 			var y = (this.dialRadius) * sin(nextTickAngle);
-			var y1 = (this.dialRadius - 5) * sin(nextTickAngle);
+			var y1 = (this.dialRadius + 5) * sin(nextTickAngle);
 
-			var tag = (map(i, 0, 9, 0, 255)).toFixed();
+			var tag = (map(i, 0, 8, 0, 255)).toFixed();
 
+			strokeWeight(2);
 			line(x, y, x1, y1);
-			text(tag, x, y - 5);
+			strokeWeight(1);
+			text(tag, x, y - 15);
 			nextTickAngle += PI / 10;
 		}
 		pop();
